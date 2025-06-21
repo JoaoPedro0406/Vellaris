c52060181330554b38c921435cedfea03c6675db
João Pedro <joaopedrooli0406@gmail.com>
2025-06-21 15:24:55 -0300

    Initial Commit

---
+++ include/navbar.php
@@ -0,0 +1,10 @@
+ <nav>
+        <div class="conteudo-nav">
+            <img src="./img/logo.png" alt="" class="logo">
+            <ul class="links-flex">
+                <li><a class="text-color-white" href="">Quem Somos</a></li>
+                <li><a class="text-color-white" href="">FAQs</a></li>
+                <li><a class="text-color-white" href="">Contato</a></li>
+            </ul>
+        </div>
+    </nav>
